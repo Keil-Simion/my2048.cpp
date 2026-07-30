147fb516bbc067fe64af670c787646751654a5e0
Keil-Simion <keilsimion@gmail.com>
2026-07-31 03:01:17 +0800

    修改了字符画

--- src/2048.cpp
+++ src/2048.cpp
@@ -1,7 +1,13 @@
 #include "2048.hpp"
 #include "menu.hpp"
+#ifdef _WIN32
+#  include <windows.h>
+#endif
 
 int main() {
+#ifdef _WIN32
+  SetConsoleOutputCP(CP_UTF8);
+#endif
   Menu::startMenu();
   return 0;
 }
